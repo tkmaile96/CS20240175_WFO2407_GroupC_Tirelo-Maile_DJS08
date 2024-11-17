7de5e2771cb33ef0ca16e3da97cd8a5a3f6159bb
Teekay <tkmaile96@gmail.com>
2024-11-17 16:03:07 +0200

    Added the Dashboard.jsx content

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,40 @@ import "./server"
 function App() {
 
   return (
-   <>
-  
-    
-  
-   </>
+   <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route
+            path="login"
+            element={<Login />}
+          />
+
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Route>
+    </Routes>
+   </BrowserRouter>
   )
 }
+ReactDOM
+.createRoot(document.getElementById('root'))
+.render(<App />);
 
 export default App
