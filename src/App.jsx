79a0032a2010ed1751c47c858205586a437cb22f
Teekay <tkmaile96@gmail.com>
2024-11-17 14:43:01 +0200

    Added Home page component with link to 'vans' route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from './Pages/Home'
 import About from './Pages/About'
+import Header from './components/Header'
 
 function App() {
 
   return (
    <>
-
+    <Header />
     <Home />
     <About />
   
