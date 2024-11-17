402e6e766b778209cd3c81d7daa9a4db361b63f0
Teekay <tkmaile96@gmail.com>
2024-11-17 15:10:05 +0200

    Added new imports and removed React import; updated App component to remove hardcoded pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './Pages/Home'
 import About from './Pages/About'
+import Vans from './Pages/Vans/Vans'
+import VanDetail from './Pages/Vans/VanDetail'
+import Login from './Pages/Login'
+import Dashboard from './Pages/Hosts/Dashboard'
+import Income from './Pages/Hosts/Income'
+import Reviews from './Pages/Hosts/Reviews'
+import HostVans from './Pages/Hosts/HostVans'
+import HostVanDetail from './Pages/Hosts/HostVansDetails'
+import HostVanInfo from './Pages/Hosts/HostVanInfo'
+import HostVanPricing from './Pages/Hosts/HostVanPricing'
+import HostVanPhotos from './Pages/Hosts/HostVanPhotos'
+import NotFound from './Pages/NotFound'
+import Layout from './Components/Layout'
+import HostLayout from './Components/HostLayout'
+import AuthRequired from './Components/AuthRequired'
+
+import "./server"
 
 
 function App() {
@@ -9,8 +27,7 @@ function App() {
   return (
    <>
   
-    <Home />
-    <About />
+    
   
    </>
   )
