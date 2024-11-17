400d8bc02635ebf0b2943f6ff7d7a9507cc36564
Teekay <tkmaile96@gmail.com>
2024-11-17 14:52:36 +0200

    Removed Header import and component, added imports for ReactDOM and react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home'
 import About from './Pages/About'
-import Header from './components/Header'
+
 
 function App() {
 
   return (
    <>
-    <Header />
+  
     <Home />
     <About />
   
